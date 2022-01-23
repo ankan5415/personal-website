3fde6e425c6e31747db925a7dba7afe0ec15795f
ankan5415 <anks5415@gmail.com>
2022-01-23 01:06:05 -0500

    finished setting up notion, chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/catamaran/300.css";
+import "@fontsource/catamaran/400.css";
+import "@fontsource/catamaran/500.css";
+import "@fontsource/catamaran/600.css";
+import "@fontsource/catamaran/700.css";
+import "@fontsource/catamaran/800.css";
+import "@fontsource/catamaran/900.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
